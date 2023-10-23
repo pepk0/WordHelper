0e450d202504ae981d5a58cf5597a52dd9c6e346
Петър Попов <popovstz@gmail.com>
2023-10-23 19:57:49 +0300

    refactor, added docstrings clarified comments

--- utils/functionality.py
+++ utils/functionality.py
@@ -1,30 +1,38 @@
-from itertools import permutations as permut
+from itertools import permutations
 
 
 def get_words(characters: str, digits: int) -> list:
-    # create all possible word combinations
-    permutations = permut(characters, digits)
-    result = ["".join(word) for word in permutations]
+    """Takes a sequence of characters and a length, 
+    returns a list if all  permutations.
+    """
+    letter_combinations = permutations(characters, digits)
+    result = ["".join(word) for word in letter_combinations]
     return result
 
 
 def validate_words(prev_searches: dict, characters: str, length: int) -> str:
-    path = r"words.txt"
+    """Validates sequence of letters.
+    
+    Arguments:
+    prev_searches -- dictionary, storing previous results of this function
+    characters -- sequence of letters 
+    length -- length of desired word validations
+    returns: string of all validated words matching the arguments
+    """
     if len(characters) == 0 or len(characters) < length:
         return "You need more Cyrillic letters"
-    # If a number that is more than the range is picked,
-    # A default 3-letter one is chosen
+    # If a number that is more than the range, 
+    # length set to 3 letters
     if length < 3 or length > 6:
         length = 3
-    # check our previous queries before running the search,
-    # to reduce response time
+    # check our previous queries
     if (characters, length) in prev_searches:
         return prev_searches[(characters, length)]
     else:  # no cached results for our query
         result = ""
         words = get_words(characters, length)
         try:
-            with open(path, "r", encoding="utf-8") as txt_file:
+            with open(r"words.txt", "r", encoding="utf-8") as txt_file:
                 for word in txt_file:
                     word = word.strip()
                     if word in words:
